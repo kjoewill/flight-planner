44830cc7a2fbc6c51f07d62f0f6e4b9807d6ff02
github username <github email address>
2019-02-20 15:04:19 -0700

    Added geo-distance calc method to Flight and corresponding tests

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,5 @@
 class Flight < ActiveRecord::Base
+  include Math
 
   belongs_to :pilot
   belongs_to :airplane
@@ -6,5 +7,35 @@ class Flight < ActiveRecord::Base
   belongs_to :destination_airport, :class_name => 'Airport', :foreign_key => 'destination_airport_id'
 
   validates :date, presence: true
-  
+
+  #Provide the distance (in nm) between the origin and desination airports
+  def distance
+    o_pos = {:latitude => origin_airport.latitude, :longitude => origin_airport.longitude}
+    d_pos = {:latitude => destination_airport.latitude, :longitude => destination_airport.longitude}
+    haversine_distance(o_pos, d_pos)
+  end
+
+private
+
+  #Employ the haversine formula to calulate the distance
+  #beteeen two lat/lon pairs provided in deciml degrees
+  def haversine_distance(from, to)
+    earth_radius = 6371 #nm
+    d_lat = to_rad(from[:latitude] - to[:latitude])
+    d_lon = to_rad(from[:longitude] - to[:longitude])
+    lat1 = to_rad(from[:latitude])
+    lat2 = to_rad(to[:latitude])
+    lon1 = to_rad(from[:longitude])
+    lon2 = to_rad(to[:longitude])
+
+    a = sin(d_lat / 2) * sin(d_lat / 2) + sin(d_lon / 2) *
+      sin(d_lon / 2) * cos(lat1) * cos(lat2)
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    earth_radius * c / 1.852 #Division is to convert from kilometers to Nautical miles
+  end
+
+  def to_rad(in_degrees)
+    in_degrees / 180 * Math::PI
+  end
+
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,28 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Flight, :type => :model do
+
+  let (:pilot) {Pilot.create(:username => 'kjoewill', :password => 'kevin')}
+  let (:kfly_airport) {Airport.create(:code => 'KFLY', :latitude => 38.945748, :longitude => -104.569893)}
+  let (:kcos_airport) {Airport.create(:code => 'KCOS', :latitude => 38.805805, :longitude => -104.700777)}
+  let (:klga_airport) {Airport.create(:code => 'KLGA', :latitude => 40.777250, :longitude => -73.872611)}
+  let (:airplane) {Airplane.create(:tail_number => 'N91971', :cruise_speed => 55)}
+  let (:flight) {Flight.create(:airplane => airplane, :pilot => pilot, :origin_airport => kfly_airport, :destination_airport => kcos_airport, :date => Date.today)}
+
   it "is valid with valid attributes and can be saved, retrieved and associations accessed" do
 
-    pilot = Pilot.create(:username => 'kjoewill', :password => 'kevin')
-    origin_airport = Airport.create(:code => 'KFLY', :latitude => 38.945748, :longitude => -104.569893)
-    destination_airport = Airport.create(:code => 'KCOS', :latitude => 38.805805, :longitude => -104.700777)
-    airplane = Airplane.create(:tail_number => 'N91971', :cruise_speed => 55)
-
-    expect(Flight.count).to eq 0
-    flight = Flight.create(:airplane => airplane, :pilot => pilot, :origin_airport => origin_airport, :destination_airport => destination_airport, :date => Date.today)
+    expect(Flight.count).to eq 0 #flight method created in "let" clause is only called when referenced below
     expect(flight).to be_valid
     expect(Flight.count).to eq 1
 
     flight.reload # = Flight.find(1)
     expect(flight.date).to eq(Date.today)
     expect(flight.airplane).to eq(airplane)
-    expect(flight.destination_airport).to eq(destination_airport)
+    expect(flight.destination_airport).to eq(kcos_airport)
     expect(flight.origin_airport.code).to eq("KFLY")
     expect(flight.destination_airport.code).to eq("KCOS")
 
     expect(flight.pilot.flights).to include(flight)
     expect(flight.airplane.pilots).to include(flight.pilot)
 
+  end
+
+  it "knows the distance between its origin and destination airports when they are close" do
+    expect(flight.distance).to be_within(0.01).of(10.39)
+  end
+
+  it "knows the distance between its origin and destination airports when they are far" do
+    flight.destination_airport = klga_airport
+    flight.save
+    flight.reload
+    expect(flight.distance).to be_within(0.1).of(1411.7)
   end
 
 end
