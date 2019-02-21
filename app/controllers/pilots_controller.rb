37c22bed439079e14a829944f83cec8bd8267cfc
github username <github email address>
2019-02-21 14:31:45 -0700

    Corrected typo in Sessions controller. added link to registration from the login page.

--- app/controllers/pilots_controller.rb
+++ app/controllers/pilots_controller.rb
@@ -7,7 +7,7 @@ class PilotsController < ApplicationController
   def create
     @pilot = Pilot.new
     @pilot.username = params[:pilot][:username]
-    @pilot.password = params[:pilot][:passwordexit]
+    @pilot.password = params[:pilot][:password]
     if @pilot.save
       session[:user_id] = @pilot.id
       redirect_to root_path
