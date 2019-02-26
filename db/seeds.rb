32d383ce96dd0c2752e4859f1d8aef8f65d53a1a
github username <github email address>
2019-02-26 14:14:12 -0700

    Initial db:seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+kfly = Airport.create(code: 'KFLY', latitude: 38.945748, longitude: -104.569893)
+kcos = Airport.create(code: 'KCOS', latitude: 38.805805, longitude: -104.700777)
+klic = Airport.create(code: 'KLIC', latitude: 39.274777, longitude: -103.665861)
+kgla = Airport.create(code: 'KLGA', latitude: 40.777250, longitude: -73.872611)
+
+cub = Airplane.create(tail_number: 'N91971', cruise_speed: 55)
+da20 = Airplane.create(tail_number: 'N921BD', cruise_speed: 120)
+sr22 = Airplane.create(tail_number: 'N652MH', cruise_speed: 200)
+
+kevin = Pilot.create(username: 'Kevin', password: 'kevin')
+tanya = Pilot.create(username: 'Tanya', password: 'tanya')
+k2    = Pilot.create(username: 'K2',    password: 'k2')
+
+
+flights = Flight.create([
+  {pilot_id: kevin.id, airplane_id: cub.id,  origin_airport_id: kfly.id, destination_airport_id: kcos.id, date: Date.today},
+  {pilot_id: kevin.id, airplane_id: da20.id, origin_airport_id: kcos.id, destination_airport_id: klic.id, date: Date.today},
+  {pilot_id: kevin.id, airplane_id: sr22.id, origin_airport_id: kcos.id, destination_airport_id: kgla.id, date: Date.today},
+
+  {pilot_id: tanya.id, airplane_id: cub.id,  origin_airport_id: kfly.id, destination_airport_id: kcos.id, date: Date.today},
+  {pilot_id: tanya.id, airplane_id: da20.id, origin_airport_id: kcos.id, destination_airport_id: klic.id, date: Date.today},
+  {pilot_id: tanya.id, airplane_id: sr22.id, origin_airport_id: kcos.id, destination_airport_id: kgla.id, date: Date.today},
+
+
+  ])
